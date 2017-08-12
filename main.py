8e601781d5ecb959bcaccdf4874cea5d93a00715
James <james@hale.com.au>
2017-08-13 09:17:02 +1000

    Initial commit

---
+++ main.py
@@ -0,0 +1,203 @@
+import requests
+import json
+import re
+import urllib2
+import sys
+import hashlib
+import picasa # picasa.py in this folder
+
+def list_blogs(auth):
+	url = 'https://www.googleapis.com/blogger/v3/users/self/blogs'
+	headers = {
+		'Authorization': auth
+	}
+
+	# GET
+	try:
+		r = requests.get(url, headers=headers)
+		#print('http status: ' + str(r.status_code))
+		#print(r.text)
+		r.raise_for_status() # Raise an exception if HTTP status code not 2xx
+	except Exception as err:
+		print(err)
+		sys.exit(1)
+
+	# Get blog urls and ids	
+	json_obj = json.loads(r.text)
+
+	return json_obj['items']
+
+
+def list_posts(auth, blog_id, page_token=None, page_num=0):
+	url = "https://www.googleapis.com/blogger/v3/blogs/{0}/posts".format(blog_id)
+	headers = {
+		'Authorization': auth
+	}
+	params = {
+		'pageToken': page_token
+	}
+
+	# HTTP GET
+	r = requests.get(url, headers=headers, params=params)
+	#print('http status: ' + str(r.status_code))
+	r.raise_for_status() # Raise an exception if HTTP status code not 2xx
+	#print(r.text)
+
+	# Get json from result
+	json_obj = json.loads(r.text)
+	# Get list from json
+	results = json_obj['items']
+	print("Got page {0}.".format(page_num))
+
+	# Looks for another page of results
+	nextPageToken = None
+	try:
+		nextPageToken = json_obj['nextPageToken']
+	except:
+		pass
+
+	if nextPageToken:
+		# Recurse this function to get the next page of results
+		print("page {0} done. on to : {1}".format(str(page_num), nextPageToken))
+		next_results = list_posts(auth, blog, nextPageToken, page_num+1)
+
+		# Combine the recursed function's results with this one in to mega result
+		for item in next_results:
+			results.append(item)
+	else:
+		print('No more pages. All done.')
+
+	return results
+
+
+# Update a single post 
+# Params:
+# - post: dict var from json_obj['items']. post keys = content, url, id, etc.
+def edit_post(auth, post, debug=False):
+	# Find all urls in post content
+	regexString = "(?:'|\")(https*:\/\/.+?)(?:'|\")" # Finds URLs
+	all_urls = re.findall(regexString, post['content'])
+
+	# Filter for just the photobucket.com image links
+	photobucket_urls = []
+	allowed_suffixes = ('png','PNG','jpg','JPG','jpeg','JPEG','bmp','BMP','gif','GIF') # This is all Picasa allows
+	for url in all_urls:
+		if 'photobucket.com/' in url and url.endswith(allowed_suffixes):
+			#print('Found URL: ' + url)
+			if "https://" in url:
+				url = url.replace("https://", "http://")
+				#print('Replacing with: ' + url) # Some posts have both a HTTPS img src and a HTTPS link
+			photobucket_urls.append(url)
+
+	photobucket_photos_de_duped = list(set(photobucket_urls))
+
+	# For each image, download from photobucket, upload to picasa, and update the image link in the post content
+	for url in photobucket_photos_de_duped:
+		# Download image from photobucket
+		req = urllib2.Request(url)
+		req.add_header('Referer', url + '.html') # The referer HTTP header is how photobucket prevents hotlinking/downloading your images
+		response = urllib2.urlopen(req)
+		image_binary = response.read()
+
+		# Check to make sure we haven't accidentally downloaded the photobucket pay-up notice image
+		md5_hash = hashlib.md5(image_binary).hexdigest()
+		if md5_hash == 'aafa26a6610d377d8e42f44bc7e76635':
+			raise Exception("It looks like something's gone wrong and we've accidentally downloaded the photobucket pay-up notice image.")
+
+		# Upload image to picasa
+		filename = url.split("/")[-1:][0] # Grabs everythign after the last slash
+		google_image_url = picasa.upload_image_to_picasa(auth, image_binary, filename)
+
+		# Update URLs in blog post content from photobucket to google
+		post['content'] = post['content'].replace(url, google_image_url)
+		# Also update the httpS version of any such url
+		post['content'] = post['content'].replace(url.replace("http://","https://"), google_image_url)
+
+	# Were any photos updated?
+	if len(photobucket_photos_de_duped) > 0:
+		#print('The following links have been replaced with Google Photos:')
+		#print(str(photobucket_photos_de_duped))
+		# Save content changes. Or if debug=True then it'll just print the list of proposed changes
+		if debug:
+			print('Not saving changes - this was a debug/test run. New content would be: ')
+			print(post['content'])
+		else:
+			print('Saved changes. See result at: ' + post['url'])
+
+		return True
+	else:
+		return False
+
+
+# Update all posts for a single blog
+def edit_all_posts(auth, all_blog_posts, debug=False):
+	for single_post in all_blog_posts:
+		try:
+			edit_post(auth_token, single_post, debug)
+		except requests.exceptions.HTTPError as err:
+			print 'ERROR: ' + err
+
+
+def find_and_edit_single_post(auth, all_blog_posts, debug=False):
+	processed_index = None
+	for index, single_post in enumerate(all_blog_posts):
+
+		actually_changed_something = edit_post(auth, single_post, False)
+
+		# problem with this is that it'll forever look at the one where nothing 
+		if actually_changed_something:
+			processed_index = index
+			break
+
+	if processed_index:
+		return {'success': True, 'processed_index': processed_index}
+	else:
+		return {'success': False}
+
+
+# Main function - program entry point
+if __name__ == '__main__':
+	print("")
+	print("INSTRUCTIONS:")
+	print("First, backup your blog. Blogger->Settings->Other->Import & back up")
+	print('Go to https://developers.google.com/oauthplayground/')
+	print('Under Step 1, select:')
+	print('- Blogger: https://www.googleapis.com/auth/blogger, and')
+	print('- Picasa: https://picasaweb.google.com/data/')
+	print('Click Authorize APIs. Follow prompts to log in with the Google account you use for Blogger')
+	print('Under Step 2, click "Exchange authorization code for tokens"')
+	auth_token = 'Bearer ' + raw_input("Paste the Access token here, and press enter: (It'll start with something like ya29.GlulBJ...): ")
+
+	all_my_blogs = list_blogs(auth_token)
+	print("")
+	print("Here's a list of your blogs: ")
+	for index, blog in enumerate(all_my_blogs):
+		print("- Blog number: {0}, url: {1}".format(index, blog['url']))
+
+	print("")
+	blog_num = raw_input("Type the number of the blog you want to work on: Default [0]: ")
+	if not blog_num:
+		blog_num = 0
+
+	print("Gathering list of all blog posts. This can take a while...")
+	all_blog_posts = list_posts(auth_token, all_my_blogs[blog_num]['id'])
+
+	print("")
+	raw_input("OK, now we're going to process/update a single blog post. This is for real. It will edit your blog live. Press ENTER to proceed. Press CTRL+C to exit.")
+
+	while True:
+		single_post_edit_result = find_and_edit_single_post(auth_token, all_blog_posts)
+		if single_post_edit_result['success']:
+			processed_index = single_post_edit_result['processed_index']
+			del all_blog_posts[processed_index] # Remove the one we've already processed so it doesn't try again on the next loop
+		else:
+			print("No remaining posts with photobucket links. I think we're all done!")
+			break
+
+		do_one_or_all = raw_input("Please check the URL above in a browser. If you're happy... Press ENTER to do one more, or type ALL and press enter to do the rest, or pretty CTRL+C to exit: ") # Blank = str = ''
+		if do_one_or_all.lower() == "all":
+			edit_all_posts(auth_token, all_blog_posts)
+			print("All posts edited.")
+			break
+
+	raw_input("Press enter to exit.")
---
+++ picasa.py
@@ -0,0 +1,32 @@
+import requests
+from xml.etree import ElementTree
+import uuid
+from datetime import datetime
+
+# Uploads image, returns image embed url
+def upload_image_to_picasa(auth, image_binary, filename):
+	extension = filename.split('.')[-1].lower()
+	if extension == 'jpg':
+		extension = 'jpeg'
+	content_type = 'image/' + extension
+
+	url = 'https://picasaweb.google.com/data/feed/api/user/default/albumid/default'
+	headers = {
+		'Content-Type': content_type, 
+		'Slug': 'Blog Import ' + datetime.now().strftime('%Y-%m-%d') + ' - ' + filename + ' - ' + str(uuid.uuid4().hex), # Unique filename because there will be dupes
+		'Authorization': auth, 
+		'Gdata-version': '2'
+	}
+
+	# POST the image
+	r = requests.post(url, headers=headers, data=image_binary)
+
+	# print(r.text)
+	print('file upload http status: ' + str(r.status_code)) # 201 = Created
+	r.raise_for_status()
+
+	# Get URL of uploaded image
+	e = ElementTree.fromstring(r.text)
+	google_image_url = e.find('{http://www.w3.org/2005/Atom}content').attrib['src']
+
+	return google_image_url
